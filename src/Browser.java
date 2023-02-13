110eee27ce8bf0ca3c5c454e084906bd1f0c8b61
brijesh-15609 <brijesh-15609@brijesh-15609.csez.zohocorpin.com>
2023-02-13 16:26:41 +0530

    with main class

---
+++ src/Browser.java
@@ -0,0 +1,55 @@
+
+import java.util.Arrays;
+
+
+
+import java.util.ArrayList;
+
+public class Browser {
+    private static  String url;
+    private static ArrayList<String> history =new ArrayList<>();
+    
+    
+    
+	public Browser() {}
+    
+    @Override
+	public String toString() {
+		return "Browser [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+				+ "]";
+	}
+
+
+    
+	public static String getUrl() {
+		return url;
+	}
+
+
+	public static void setUrl(String url) {
+		Browser.url = url;
+	}
+
+
+
+	public static ArrayList<String> getHistory() {
+		return history;
+	}
+
+
+
+	public static void setHistory(ArrayList<String> history) {
+		Browser.history = history;
+	}
+
+	
+
+	public  Browser(ArrayList<String> history) {
+        this.history = history;
+        history.add(url);
+        System.out.println("new history is "+history);
+    }
+	
+}
+
+
---
+++ src/MainBrowser.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainBrowser {
+	public static void main(String[] args) {
+	    Browser br = new Browser();
+	    br.setUrl("https://www.google.com");
+	
+	    ArrayList<String> newHistory = new ArrayList<>();
+	    
+	    newHistory.add("https://www.facebook.com");
+	    newHistory.add("https://www.github.com");
+	    br.setHistory(newHistory);
+	    
+	    System.out.println("Browser history: " + br.getHistory());
+	    Browser brow = new Browser(newHistory);
+	}
+}
+
