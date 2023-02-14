b99d300b0735276e11117aa75b81704ba847abe9
brijesh-15609 <brijesh-15609@brijesh-15609.csez.zohocorpin.com>
2023-02-14 12:24:33 +0530

    1st modification

--- src/Browser.java
+++ src/Browser.java
@@ -6,46 +6,70 @@ import java.util.Arrays;
 import java.util.ArrayList;
 
 public class Browser {
-    private static  String url;
-    private static ArrayList<String> history =new ArrayList<>();
+    private ArrayList<String> history =new ArrayList<>();
+    static ArrayList<String> history_1 =new ArrayList<>();
+    private ArrayList<String> history_2 =new ArrayList<>();
+    ArrayList<String> history_3 =new ArrayList<>();
     
     
-    
+	public static ArrayList<String> getHistory_1() {
+		return history_1;
+	}
+
+	public static void setHistory_1(ArrayList<String> history_1) {
+		Browser.history_1 = history_1;
+	}
+
+	public ArrayList<String> getHistory_2() {
+		return history_2;
+	}
+
+	public void setHistory_2(ArrayList<String> history_2) {
+		this.history_2 = history_2;
+	}
+
+	public ArrayList<String> getHistory_3() {
+		return history_3;
+	}
+
+	public void setHistory_3(ArrayList<String> history_3) {
+		this.history_3 = history_3;
+	}
+	
+	
+	
+	
+	
+
 	public Browser() {}
     
+	
+	
+	
+	
+	
     @Override
 	public String toString() {
-		return "Browser [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
-				+ "]";
+		return "Browser [history_2=" + history_2 + ", history_3=" + history_3 + ", history_1=" + history_1 + ", history=" + history +"]";
 	}
     
     
-    
+   
 
-	public static String getUrl() {
-		return url;
-	}
-public static void setUrl(String url) {
-		Browser.url = url;
-	}
-
-	public static ArrayList<String> getHistory() {
+	public ArrayList<String> getHistory() {
 		return history;
 	}
 
 
 
-	public static void setHistory(ArrayList<String> history) {
-		Browser.history = history;
+	public void setHistory(ArrayList<String> history) {
+		this.history = history;
 	}
 
 	
 
 	public  Browser(ArrayList<String> history) {
-        this.history = history;
-        history.add(url);
-        
-        System.out.println("new history is "+history);
+        this.history = history; 
     }
 	
 }
--- src/MainBrowser.java
+++ src/MainBrowser.java
@@ -2,19 +2,26 @@ import java.util.ArrayList;
 
 public class MainBrowser {
 	public static void main(String[] args) {
-	    Browser br = new Browser();
-	    br.setUrl("https://www.google.com");
-	
+	    Browser zoho = new Browser();
 	    ArrayList<String> newHistory = new ArrayList<>();
-	           
-	   
-	    
+	         
 	    newHistory.add("https://www.facebook.com");
 	    newHistory.add("https://www.github.com");
-	    br.setHistory(newHistory);
+	   
+	    zoho.setHistory(newHistory);
 	    
-	    System.out.println("Browser history: " + br.getHistory());
-	    Browser brow = new Browser(newHistory);
+	    zoho.setHistory_1(newHistory);
+	    zoho.setHistory_2(newHistory);
+	    zoho.setHistory_3(newHistory);
+	   
+	   
+	    System.out.println("Browser history: " + zoho.getHistory());
+	    System.out.println("Browser history_1 is: " + zoho.getHistory_1());
+	    System.out.println("Browser history_2 is: " + zoho.getHistory_2());
+	    System.out.println("Browser history_3 is: " + zoho.getHistory_3());
+	    Browser browser = new Browser(newHistory);
+	    System.out.println(browser);
+	    System.out.println(zoho);
 	}
 }
 
